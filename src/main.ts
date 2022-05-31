22d09ceaeea488a192ea3bddd6cfc880371a31e7
Bennett Benedict <bennybenester@gmail.com>
2022-05-31 11:23:24 +0300

    add test webhook sms

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import * as github from '@actions/github'
 import * as core from '@actions/core'
 import axios from 'axios'
-
-const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
-const SLACK_URL = core.getInput('SLACK_URL')
-const ASSETS_LOCATION = core.getInput('ASSETS_LOCATION')
-const GITHUB_RELEASES = core.getInput('GITHUB_RELEASES')
-const octokit = github.getOctokit(GITHUB_TOKEN)
+// const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+// const SLACK_URL = core.getInput('SLACK_URL')
+// const ASSETS_LOCATION = core.getInput('ASSETS_LOCATION')
+// const GITHUB_RELEASES = core.getInput('GITHUB_RELEASES')
+// const APP_NAME = core.getInput('APP_NAME')
+const APP_NAME = 'TEST'
+const SLACK_WEBHOOK_URL = core.getInput('SLACK_WEBHOOK_URL')
+// const octokit = github.getOctokit(GITHUB_TOKEN)
 const {context = {}}: any = github
 
 const run = async () => {
@@ -22,37 +25,26 @@ const run = async () => {
 }
 
 const releaseFromPR = async (): Promise<void> => {
-  const newFeatures = context?.payload?.commits?.filter(({message}) =>
-    message.includes('feat')
-  )
-  const bugFixes = context?.payload?.commits?.filter(
-    ({message}) => message.includes('bug') || message.includes('fix')
-  )
-  const docs = context?.payload?.commits?.filter(({message}) =>
-    message.includes('docs')
-  )
-  const uncategorized = context?.payload?.commits?.filter(
-    ({message}) =>
-      !message.includes('bug') ||
-      !message.includes('fix') ||
-      !message.includes('feat') ||
-      !message.includes('chore') ||
-      !message.includes('docs')
-  )
-  // await axios.post(SLACK_URL)
-}
-const releaseFromPush = async (): Promise<void> => {
-  try {
-    const newFeatures = context?.payload?.commits?.filter(({message}) =>
-      message.includes('feat')
+  const newFeatures = context?.payload?.commits
+    ?.filter(({message}) => message.includes('feat'))
+    ?.map(
+      (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
     )
-    const bugFixes = context?.payload?.commits?.filter(
-      ({message}) => message.includes('bug') || message.includes('fix')
+    .join('\n\n > ')
+  const bugFixes = context?.payload?.commits
+    ?.filter(({message}) => message.includes('bug') || message.includes('fix'))
+    ?.map(
+      (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
     )
-    const docs = context?.payload?.commits?.filter(({message}) =>
-      message.includes('docs')
+    .join('\n\n > ')
+  const docs = context?.payload?.commits
+    ?.filter(({message}) => message.includes('docs'))
+    ?.map(
+      (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
     )
-    const uncategorized = context?.payload?.commits?.filter(
+    .join('\n\n > ')
+  const uncategorized = context?.payload?.commits
+    ?.filter(
       ({message}) =>
         !message.includes('bug') ||
         !message.includes('fix') ||
@@ -60,8 +52,127 @@ const releaseFromPush = async (): Promise<void> => {
         !message.includes('chore') ||
         !message.includes('docs')
     )
+    ?.map(
+      (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
+    )
+    .join('\n\n > ')
+  const options = getWebHookOptions({
+    newFeatures,
+    docs,
+    bugFixes,
+    uncategorized
+  })
+  await axios.post(SLACK_WEBHOOK_URL, JSON.stringify(options))
+}
+const releaseFromPush = async (): Promise<void> => {
+  try {
+    const newFeatures = context?.payload?.commits
+      ?.filter(({message}) => message.includes('feat'))
+      ?.map(
+        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
+      )
+      .join('\n\n > ')
+    const bugFixes = context?.payload?.commits
+      ?.filter(
+        ({message}) => message.includes('bug') || message.includes('fix')
+      )
+      ?.map(
+        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
+      )
+      .join('\n\n > ')
+    const docs = context?.payload?.commits
+      ?.filter(({message}) => message.includes('docs'))
+      ?.map((commit: {message: string}, i: number) => `${i}. ${commit.message}`)
+    const uncategorized = context?.payload?.commits
+      ?.filter(
+        ({message}) =>
+          !message.includes('bug') ||
+          !message.includes('fix') ||
+          !message.includes('feat') ||
+          !message.includes('chore') ||
+          !message.includes('docs')
+      )
+      ?.map(
+        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
+      )
+      .join('\n\n > ')
+    const options = getWebHookOptions({
+      newFeatures,
+      docs,
+      bugFixes,
+      uncategorized
+    })
+    await axios.post(SLACK_WEBHOOK_URL, JSON.stringify(options))
   } catch (e) {
     core.setFailed(e.message)
+  }
+}
+
+const getWebHookOptions = ({newFeatures, docs, bugFixes, uncategorized}) => {
+  return {
+    blocks: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: `:sparkles: New version released on *${APP_NAME}*`,
+          emoji: true
+        }
+      },
+      {
+        type: 'context',
+        elements: [
+          {
+            text: ` *${APP_NAME}*`,
+            type: 'mrkdwn'
+          }
+        ]
+      },
+      {
+        type: 'divider'
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `New Features`,
+          fields: [{type: 'mrkdwn', text: newFeatures}]
+        }
+      },
+      {
+        type: 'divider'
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Bug Fixes`,
+          fields: [{type: 'mrkdwn', text: bugFixes}]
+        }
+      },
+      {
+        type: 'divider'
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Documentation`,
+          fields: [{type: 'mrkdwn', text: docs}]
+        }
+      },
+      {
+        type: 'divider'
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Miscellaneous`,
+          fields: [{type: 'mrkdwn', text: uncategorized}]
+        }
+      }
+    ]
   }
 }
 run()
