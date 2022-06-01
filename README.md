7b9f25c29e3ac78407059fce0fb1f5f529b751b7
Bennett Benedict <bennybenester@gmail.com>
2022-06-01 13:41:37 +0300

    improve documantation in the README

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# demo
+![Workflow Status](https://github.com/BaharaJr/releases/actions/workflows/test.yml/badge.svg)
 
-demo repository
+# Automatically Send Releases to Slack and GitHub Assets
+
+## Requirements
+
+- This runner accepts the following parameters.
+
+|        Key        |                         Description                               | Required   |      Required Condition       |
+| ----------------- | ----------------------------------------------------------------- | ---------- | ----------------------------- |
+| GITHUB_TOKEN      | A string GitHub token from a user with accesss to the repo.       |     NO     | Assets is true                |
+| ASSETS_LOCATION   | A string location of the assets if sending to slack or GitHub.    |     NO     | Assets is true                |
+| ASSETS            | A boolean dictating whether or not to add GitHub Assets.          |     NO     |                               |
+| SLACK_WEBHOOK_URL | Slack webhook URL for sending release summaries to slack.         |     NO     |                               |
+| SLACK_ASSET       | A boolean dictating whether to send assets to slack or not.       |     NO     |                               |
+| SLACK_CHANNEL     | Slack channel to send assets.                                     |     NO     | Slack asset is true           |
+| APP_NAME          | Your application/asset name for reference                         |     YES    |                               |
+
+## Roadmap
+
+- We look forward to adding more functions in the future, feel free to drop an issue for any features you might want added.
+
+## Contributing
+
+Contributions are always welcome and highly encourage!
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,47 @@ import * as github from '@actions/github'
 import * as core from '@actions/core'
 import axios from 'axios'
 // const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
-// const SLACK_URL = core.getInput('SLACK_URL')
-// const ASSETS_LOCATION = core.getInput('ASSETS_LOCATION')
-// const GITHUB_RELEASES = core.getInput('GITHUB_RELEASES')
-// const APP_NAME = core.getInput('APP_NAME')
-const APP_NAME = 'TEST'
-const SLACK_WEBHOOK_URL = core.getInput('SLACK_WEBHOOK_URL')
 // const octokit = github.getOctokit(GITHUB_TOKEN)
+// const SLACK_UPLOADS = 'https://slack.com/api/files.upload'
+// const INITIAL_COMMENT = core.getInput('INITIAL_COMMENT')
+const SLACK_CHANNEL = core.getInput('SLACK_CHANNEL')
+const SLACK_ASSET = core.getInput('SLACK_ASSET')
+const ASSETS_LOCATION: string = core.getInput('ASSETS_LOCATION')
+const ASSETS = core.getInput('ASSETS')
+const GITHUB_RELEASES = core.getInput('GITHUB_RELEASES')
+const APP_NAME = core.getInput('APP_NAME')
+const SLACK_WEBHOOK_URL = core.getInput('SLACK_WEBHOOK_URL')
 const {context = {}}: any = github
 
+const newFeatures = context?.payload?.commits
+  ?.filter(({message}) => message.includes('feat'))
+  ?.map((commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`)
+  .join('\n\n > ')
+
+const bugFixes = context?.payload?.commits
+  ?.filter(({message}) => message.includes('bug') || message.includes('fix'))
+  ?.map((commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`)
+  .join('\n\n > ')
+
+const docs = context?.payload?.commits
+  ?.filter(({message}) => message.includes('docs'))
+  ?.map((commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`)
+  .join('\n\n > ')
+
+const uncategorized = context?.payload?.commits
+  ?.filter(
+    ({message}) =>
+      !message.includes('bug') &&
+      !message.includes('fix') &&
+      !message.includes('feat') &&
+      !message.includes('chore') &&
+      !message.includes('docs')
+  )
+  ?.map((commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`)
+  .join('\n\n > ')
+
 const run = async () => {
+  checkParams()
   const eventName = context.eventName
   switch (eventName) {
     case 'push':
@@ -24,102 +55,55 @@ const run = async () => {
   }
 }
 
+const checkParams = () => {
+  if (!SLACK_WEBHOOK_URL && !GITHUB_RELEASES) {
+    core.setFailed('Atleast one functionality is required')
+    return
+  }
+
+  if (GITHUB_RELEASES && ASSETS && !ASSETS_LOCATION) {
+    core.setFailed('GitHub Assets require an asset location')
+    return
+  }
+
+  if (SLACK_ASSET && !SLACK_CHANNEL) {
+    core.setFailed('Slack channel is required to send  Assets')
+    return
+  }
+}
+
 const releaseFromPR = async (): Promise<void> => {
   try {
-    const newFeatures = context?.payload?.commits
-      ?.filter(({message}) => message.includes('feat'))
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const bugFixes = context?.payload?.commits
-      ?.filter(
-        ({message}) => message.includes('bug') || message.includes('fix')
-      )
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const docs = context?.payload?.commits
-      ?.filter(({message}) => message.includes('docs'))
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const uncategorized = context?.payload?.commits
-      ?.filter(
-        ({message}) =>
-          !message.includes('bug') ||
-          !message.includes('fix') ||
-          !message.includes('feat') ||
-          !message.includes('chore') ||
-          !message.includes('docs')
-      )
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const options = getWebHookOptions({
-      newFeatures,
-      docs,
-      bugFixes,
-      uncategorized
-    })
-    await axios.post(SLACK_WEBHOOK_URL, JSON.stringify(options))
+    if (SLACK_WEBHOOK_URL) {
+      const options = getWebHookOptions()
+      await axios.post(SLACK_WEBHOOK_URL, JSON.stringify(options))
+    }
+    if (SLACK_ASSET) {
+      core.warning('Slack Assets are currently not supported')
+      /*
+      const formData = new FormData()
+      formData.append('file', readFileSync(ASSETS_LOCATION))
+      formData.append('initial_comment', INITIAL_COMMENT || '')
+      formData.append('channels', SLACK_CHANNEL)
+
+      await axios.post(SLACK_UPLOADS)
+      */
+    }
+    return
   } catch (e) {
-    console.log(e)
     core.setFailed('Failed to send to slack')
   }
 }
 const releaseFromPush = async (): Promise<void> => {
   try {
-    const newFeatures = context?.payload?.commits
-      ?.filter(({message}) => message.includes('feat'))
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const bugFixes = context?.payload?.commits
-      ?.filter(
-        ({message}) => message.includes('bug') || message.includes('fix')
-      )
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const docs = context?.payload?.commits
-      ?.filter(({message}) => message.includes('docs'))
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const uncategorized = context?.payload?.commits
-      ?.filter(
-        ({message}) =>
-          !message.includes('bug') ||
-          !message.includes('fix') ||
-          !message.includes('feat') ||
-          !message.includes('chore') ||
-          !message.includes('docs')
-      )
-      ?.map(
-        (commit: {message: string}, i: number) => `${i + 1}. ${commit.message}`
-      )
-      .join('\n\n > ')
-    const options = getWebHookOptions({
-      newFeatures,
-      docs,
-      bugFixes,
-      uncategorized
-    })
+    const options = getWebHookOptions()
     await axios.post(SLACK_WEBHOOK_URL, JSON.stringify(options))
   } catch (e) {
-    console.log(e)
     core.setFailed('Failed to send to slack')
   }
 }
 
-const getWebHookOptions = ({newFeatures, docs, bugFixes, uncategorized}) => {
+const getWebHookOptions = () => {
   return {
     blocks: [
       {
